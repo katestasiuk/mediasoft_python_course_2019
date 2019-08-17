acc295c2dc2e49c236a65973baa6009145183527
katestasiuk <k.ryzhenkova@gmail.com>
2019-08-17 12:56:59 +0300

    Add files via upload

--- timenzone_telegram_bot/timenzone.py
+++ timenzone_telegram_bot/timenzone.py
@@ -11,23 +11,19 @@ dt = datetime.datetime.now()
 fmt = '%H:%M:%S %Z%z'
 apihelper.proxy = {'https':'23.237.173.102:3128'}
 bot = telebot.TeleBot('920238732:AAHXx2uahTn45Txb7WyFUJX5wJkkQgbssIY')
-markup = types.InlineKeyboardMarkup(row_width=2)
-itembtn1 = types.InlineKeyboardButton(text = 'My timezone', callback_data = 'my')
-itembtn2 = types.InlineKeyboardButton(text = 'I will enter a city', callback_data = 'city')
-markup.add(itembtn1, itembtn2)
 zones = common_timezones
 
-def several_zones(zone):
+def check_city(city):     #проверка, есть ли город в базе часовых поясов
+    for i in zones:
+        if city in i:
+            return dt.astimezone(pytz.timezone(i)).strftime(fmt)
+
+def several_zones(zone):   #обработка ситуации, когда в базе часовых поясов найдено >1 совпадения
     zones_select = []
     for i in zones:
         if zone in i:
             zones_select.append(i)
     return zones_select
-
-def check_city(city):
-    for i in zones:
-        if city in i:
-            return dt.astimezone(pytz.timezone(i)).strftime(fmt)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
@@ -35,7 +31,7 @@ def send_welcome(message):
 	
 @bot.message_handler(content_types=['text'])
 def collect_city(message):
-    converted_message = message.text.title().replace(" ", "_")
+    converted_message = message.text.title().replace(" ", "_")   #полученное сообщение приводится к виду Текст_Текст в соответствии с форматированием данных в базе часовых поясов
     zone_check = several_zones(converted_message)
     result = check_city(converted_message)
     if len(zone_check) > 1:
